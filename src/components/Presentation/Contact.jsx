f9554751c037dce08cc7354c343705b32ffbf16e
Lysard <leo.ripert@gmail.com>
2024-02-17 14:11:19 +0100

    :memo:Contact Form logic done

--- src/components/Presentation/Contact.jsx
+++ src/components/Presentation/Contact.jsx
@@ -1,8 +1,8 @@
 import s from './contact.module.css'
-import emailjs from 'emailjs-com';
+import emailjs from '@emailjs/browser'
 import { useRef, useState, useEffect } from "react"
 import ReCAPTCHA from 'react-google-recaptcha'
-import { useDarkMode } from '/src/contexts/DarkModeContext';
+import { useDarkMode } from '/src/contexts/DarkModeContext'
 
 
 export default function Contact(){
@@ -26,6 +26,7 @@ export default function Contact(){
 
     const [inputsFormOk, setInputFormOk] = useState(false)
     const [sendingMail, setSendingMail] = useState(false)
+    const [errorSendingMail, setErrorSendingMail] = useState(false)
 
     const [firstLoad, setFirstLoad] = useState(true)
     const [isBlue, setIsBlue] = useState(true)
@@ -37,7 +38,7 @@ export default function Contact(){
     }
     
     useEffect(()=>{
-        const aroundLine = document.querySelector('#aroundLine');
+        const aroundLine = document.querySelector('#aroundLine')
         if(firstLoad){
             setTimeout(()=>{
                 aroundLine.background = 'var(--blue)'
@@ -54,8 +55,8 @@ export default function Contact(){
             setInputFormOk(true)
         }else{setInputFormOk(false)}
     }, [nameIstLongEnough, emailIsValid, messageLengthOk, recaptchaOk])
-
-    const sendMail = (e) =>{
+    
+    const sendMail = e =>{
         e.preventDefault()
 
         if(!inputsFormOk){return}
@@ -64,56 +65,58 @@ export default function Contact(){
 
         setMailSent(null); setMailDivDesapear(null); setNameIstLongEnough(null); setNameLongEnough(null); setEmailIsValid(null); 
         setEmailValid(null); setMessageLengthOk(null); setMessageLongEnough(null); setInputFormOk(false); setRecaptchaOk(false);
-
-        emailjs.sendForm(process.env.REACT_APP_SERVICE_ID, process.env.REACT_APP_TEMPLATE_ID, e.target, process.env.REACT_APP_PUBLIC_KEY)
-        .then((response) => {
-            setMailSent(true);
-            console.log('Email envoyé avec succès !', response)
+        
+        emailjs.sendForm(import.meta.env.VITE_SERVICE_ID, import.meta.env.VITE_TEMPLATE_ID, e.target, import.meta.env.VITE_PUBLIC_KEY)
+        .then(response => {
+            setMailSent(true)
+            // console.log('Email envoyé avec succès !', response)
             setTimeout(()=>{
-                formRef.current.reset();
+                formRef.current.reset()
                 captchaRef.current.reset()
                 setSendingMail(false)
                 setIsBlue(true)
             }, 400)
-        }).catch((error) => {setMailSent(false); console.error('Erreur lors de l\'envoi de l\'email :', error)})
+        }).catch(error => {setMailSent(false); setErrorSendingMail(true); console.error('Erreur lors de l\'envoi de l\'email :', error)})
         setTimeout(()=>{setMailDivDesapear(true)}, 3000)
+
+        document.querySelector('#sendBtn').blur()
     }
 
 
     const [buttonText, setButtonText] = useState('COMPLÉTEZ SVP');
-    const handleMouseEnter = () => { if(!inputsFormOk){setButtonText('FORMULAIRE INVALIDE 🤨')}}
+    const handleMouseEnter = () => { if(!inputsFormOk){setButtonText('FORMULAIRE INVALIDE')}}
     const handleMouseLeave = () => { if(!inputsFormOk){setButtonText('COMPLÉTEZ SVP')}}
 
 
-    const checkUsername = (username) =>{
+    const checkUsername = username =>{
         if(username.length > 1){setNameIstLongEnough(true); setNameLongEnough(true)}
         else{setNameIstLongEnough(false)}
     }
 
-    const checkEmail = (email) =>{
+    const checkEmail = email =>{
         const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
         if(emailRegex.test(email)){setEmailIsValid(true); setEmailValid(true)}
         else{setEmailIsValid(false)}
     }
 
-    const checkMessage = (message) =>{
+    const checkMessage = message =>{
         if(message.length >= 15){setMessageLengthOk(true); setMessageLongEnough(true)}
         else{setMessageLengthOk(null)}
     }
 
     // this 3 next are on onBlur (loss focus on input), the others are onChange
-    const checkUsernameOut = (username) =>{
+    const checkUsernameOut = username =>{
         if(username.length > 1){setNameLongEnough(true)}
         else{setNameLongEnough(false)}
     }
 
-    const checkEmailOut = (email) =>{
+    const checkEmailOut = email =>{
         const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
         if(emailRegex.test(email)){setEmailValid(true)}
         else{setEmailValid(false)}
     }
 
-    const checkMessageOut = (username) =>{
+    const checkMessageOut = username =>{
         if(username.length >= 15){setMessageLongEnough(true)}
         else{setMessageLongEnough(false)}
     }
@@ -124,28 +127,28 @@ export default function Contact(){
                     {mailSent === false && <div ref={mailSentRef} className={`${s.mailNotSend} ${mailDivDesapear ? s.hide : ""}`}>Votre demande n'a pas pu aboutir !<br />Si l'erreur persiste,veuillez nous contacter par téléphone.</div>}
                     
 
-                    <form onSubmit={(e)=>sendMail(e)} className={s.form} ref={formRef} noValidate>
+                    <form onSubmit={ e =>sendMail(e)} className={s.form} ref={formRef} noValidate>
                         <label htmlFor="username">Nom</label>
-                        <input name="username" type="text" minLength="2" maxLength="50" className={`${s.username} ${nameIstLongEnough && s.inputOkay} ${nameLongEnough === false && s.inputNotOkay}`} onChange={(e) => checkUsername(e.target.value)} onBlur={(e) => checkUsernameOut(e.target.value)} />
+                        <input name="username" type="text" minLength="2" maxLength="50" className={`${s.username} ${nameIstLongEnough && s.inputOkay} ${nameLongEnough === false && s.inputNotOkay}`} onChange={ e => checkUsername(e.target.value)} onBlur={ e => checkUsernameOut(e.target.value)} />
                         {!nameLongEnough && nameLongEnough !== null && <p className={s.errorP}>Entre 2 et 50 caractères requis</p>}
 
                         <label htmlFor="email">Email</label>
-                        <input name="email" type="text" id='email' className={`${emailIsValid && s.inputOkay} ${emailValid === false && s.inputNotOkay}`} onChange={(e) => checkEmail(e.target.value)} onBlur={(e) => checkEmailOut(e.target.value)} />
+                        <input name="email" type="text" id='email' className={`${emailIsValid && s.inputOkay} ${emailValid === false && s.inputNotOkay}`} onChange={ e => checkEmail(e.target.value)} onBlur={ e => checkEmailOut(e.target.value)} />
                         {!emailValid && emailValid !== null && <p className={s.errorP}>Format attendu : 'exemple@gmail.com'</p>}
 
                         <label htmlFor="message">Message</label>
-                        <textarea name="message" rows="2" minLength="15" maxLength="500" id={messageLengthOk && 'message'} className={`${messageLengthOk && s.inputOkay} ${messageLongEnough === false && s.inputNotOkay}`} onChange={(e) => checkMessage(e.target.value)} onBlur={(e) => checkMessageOut(e.target.value)} title='Veuillez décrire votre demande : entre 15 et 500 caractères' style={{outline:'none'}} />
+                        <textarea name="message" rows="2" minLength="15" maxLength="500" id={messageLengthOk && 'message'} className={`${messageLengthOk && s.inputOkay} ${messageLongEnough === false && s.inputNotOkay}`} onChange={ e => checkMessage(e.target.value)} onBlur={ e => checkMessageOut(e.target.value)} title='Veuillez décrire votre demande : entre 15 et 500 caractères' style={{outline:'none'}} />
                         {!messageLongEnough && messageLongEnough !== null && <p className={`${s.errorP} ${s.errorMessage}`}>Décrivez votre demande: min 15 caractères</p>}
                         
 
                         {/* <label htmlFor="file">Image <span>optionnel, 500kb max.</span></label>
                         <input type="file" name='file' /> */}
                         <div style={{position: 'relative'}}>
-                            <ReCAPTCHA ref={(el) => (captchaRef.current = el)} sitekey="6Ld6ZJAnAAAAAIriI7n7-n1H2P-juUxP17QAyV8n" onChange={handleCaptchaVerification} style={{transform: 'scale(0.74) translateX(-38.5px)'}} />
-                            <div id='aroundLine' className={s.aroundLine} ></div>
+                            <ReCAPTCHA ref={ el => captchaRef.current = el} sitekey="6Ld6ZJAnAAAAAIriI7n7-n1H2P-juUxP17QAyV8n" onChange={handleCaptchaVerification} style={{transform: 'scale(0.74) translateX(-38.5px)'}} />
+                            <div id='aroundLine' className={s.aroundLine}></div>
                         </div>
-                        <button type='submit' className={sendingMail ? `${s.btn} ${s.btnSending}` : inputsFormOk ? `${s.btn} ${s.btnOk}` : `${s.btn} ${s.deactivatedBtn}`} style={{fontFamily: 'initial'}} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} >
-                            {sendingMail ? 'Envoie en cours' : inputsFormOk ? 'ENVOYER VOTRE DEMANDE' : buttonText}
+                        <button type='submit' className={sendingMail ? `${s.btn} ${s.btnSending}` : inputsFormOk ? `${s.btn} ${s.btnOk}` : `${s.btn} ${s.deactivatedBtn}`} style={{fontFamily: 'initial'}} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} id='sendBtn' >
+                            {sendingMail ? 'Envoie en cours' : errorSendingMail ? 'Echec de l\'envoie' : inputsFormOk ? 'ENVOYER VOTRE DEMANDE' : buttonText}
                         </button>
                     </form>
                 </div>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: { 
@@ -9,5 +8,6 @@ export default defineConfig({
   },
   define: {
     'process.env': {}
-  }
+  },
+  envDir: './',
 })
