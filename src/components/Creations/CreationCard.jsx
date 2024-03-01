a422976cdaa890af8b0f1b59baa7ed7add81e1d8
Lysard <leo.ripert@gmail.com>
2024-03-01 13:29:06 +0100

    :construction:Working on Creation page

--- src/components/Creations/CreationCard.jsx
+++ src/components/Creations/CreationCard.jsx
@@ -1,16 +1,30 @@
 import s from './creationCard.module.css'
+import { useState } from 'react'
 
-export default function CreationCard({ src, projectName, language, children }){
+export default function CreationCard({ src, projectName, language, children, link }){
+
+  const [mouseOver, setMouseOver] = useState(false)
+
   return(
-    <div className={s.wrapper}>
+    <div className={s.wrapper} onMouseOver={() => setMouseOver(true)} onMouseOut={() => setMouseOver(false)} >
       <div className={s.borderPurpose}>
         <div className={s.card} style={{ backgroundImage: `url(${src})` }}>
           <div className={s.content}>
-            <h1>{projectName}</h1>
+            <div>
+              <h6><span>Nom du Projet</span></h6>
+              <h1>{projectName}</h1>
+            </div>
             <br />
-            <h2>{language}</h2>
+            <div>
+              <h6><span>Technologie</span></h6>
+              <h2>{language}</h2>
+            </div>
             <br />
-            <p>{children}</p>
+            <div>
+              <h6><span>Description</span></h6>
+              <p>{children}</p>
+            </div>
+            <a href={link} target='_blank' className={`${s.link} ${mouseOver ? '' : s.opacity0}`}>Lien vers {projectName}</a>
           </div>
         </div>
       </div>
--- src/components/Creations/Creations.jsx
+++ src/components/Creations/Creations.jsx
@@ -3,6 +3,11 @@ import Footer from '../Footer/Footer.jsx'
 import CreationCard from './CreationCard.jsx'
 import { useVisited } from '/src/contexts/VisitedContext'
 import foodtruck from '/img/creations/foodtruck_representation.png'
+import fruitsMarket from '/img/creations/fruitsMarket_representation.png'
+import spa from '/img/creations/spa_representation.png'
+import gilles from '/img/creations/gilles_representation.png'
+
+
 
 export default function Creations() {
   
@@ -12,10 +17,24 @@ export default function Creations() {
 
   return (
     <div>
-      Quelques Projets parmis d'autres
-      <CreationCard src={foodtruck} projectName='Fruit Market' language='react'>
-        Site pour un foodtruck.
-      </CreationCard>
+      <div className='flex flex-col'>
+        <div className='flex justify-around mb-4'>
+          <CreationCard src={foodtruck} projectName='Foodtruck' language='react' link='https://foodtruck.leorip.com/'>
+            Site pour un foodtruck
+          </CreationCard>
+          <CreationCard src={fruitsMarket} projectName='Fruits Market' language='react' link='https://fruitsmarket.leorip.com/'>
+            Devanture d'un marché de fruits
+          </CreationCard>
+        </div>
+        <div className='flex justify-around'>
+          <CreationCard src={spa} projectName='SPA' language='PHP & SQL' link='https://spa.leorip.com/'>
+            Société protéctrice des Animaux <small className="pl-12 -mr-12 text-red-800">(<small className='line-through'>Single Page Application</small>)</small>
+          </CreationCard>
+          <CreationCard src={gilles} projectName='Gilles' language='PHP & SQL' link='https://gilles.leorip.com/'>
+            Un projet en commun,<br />dont je me suis occupé du backEnd et de l'invention du jeu
+          </CreationCard>
+        </div>
+      </div>
       <br />
       <Footer />
     </div>
